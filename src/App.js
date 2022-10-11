ac263bf947fcf1bfeb006180afbbda86edf72bf4
Imtiaj09 <ahmimtiaj@gmail.com>
2022-10-12 04:07:16 +0600

    RouterProvider has been added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import Main from './layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        }
+      ]
+
+    },
+
+
+  ])
   return (
     <div className="App">
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
