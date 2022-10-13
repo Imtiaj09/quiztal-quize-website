c45d5a693fa80e69792e45918ca0caf1441e23a4
Imtiaj09 <ahmimtiaj@gmail.com>
2022-10-13 16:49:26 +0600

    recharts has been added base on total quize

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react';
-import { Line, LineChart, XAxis, YAxis } from 'recharts';
+import {  CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import { TopicContext } from '../../layout/Main';
 
 const Statistics = () => {
   const topics = useContext(TopicContext);
   console.log(topics)
   return (
-    <div>
+    <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20'>
       <LineChart width={500} height={400} data={topics.data}>
-      <Line type="monotone" dataKey="total" stroke="#ff7300" />
+      <Line type="monotone" dataKey="total" stroke="#87CEEB" />
+      <CartesianGrid stroke="#f5f5f5" />
       <XAxis dataKey="name" scale="band" />
           <YAxis />
+          <Tooltip />
       </LineChart>
     </div>
   );
